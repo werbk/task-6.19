902863774bf21f5f4cbf90854cc865a8f049ead3
werbk <werbk@bk.ru>
2015-06-29 12:24:02 +0300

    task 6.19

---
+++ conftest.py
@@ -0,0 +1,72 @@
+import pytest
+import logging
+import json
+import jsonpickle
+import os.path
+import importlib
+from fixture.TestBase import BaseClass
+from fixture.variables import UserLogin
+
+
+fixture = None
+target = None
+
+
+@pytest.fixture
+def app(request):
+    global fixture
+    global target
+
+    browser = request.config.getoption('--browser')
+
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption('--target'))
+        with open(config_file) as file:
+            target = json.load(file)
+
+
+    #url = request.config.getoption('--baseUrl')
+    #login_user = request.config.getoption('--login_user')
+    #login_password = request.config.getoption('--login_password')
+
+    if fixture is None or not fixture.is_valid():
+        fixture = BaseClass(browser=browser, base_url=target['baseUrl'])
+
+    fixture.session.ensure_login(user_name=target['username'], password=target['password'])
+    return fixture
+
+@pytest.fixture(scope='session', autouse=True)
+def stop(request):
+
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.restore()
+    request.addfinalizer(fin)
+    return fixture
+
+
+def pytest_addoption(parser):
+    default_login_user = [UserLogin.name, UserLogin.password]
+    parser.addoption('--browser', action='store', default='firefox')
+    parser.addoption('--target', action='store', default='target.json') #'http://localhost/addressbook/')
+
+    # i believe that it possible do in 1 line but i don't know how two in 1 Login take to parameter at same time
+    #parser.addoption('--loginu', action='store', default=default_login_user[0])
+    #parser.addoption('--loginp', action='store', default=default_login_user[1])
+
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).test_data
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/%s.json' % file)) as f:
+        return jsonpickle.decode(f.read())
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
---
+++ tests_group/test_group.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from random import randrange
+
+import pytest
+
+from fixture.variables import Profinity
+from tests_group.group_helper import Group
+from data.groups import test_data
+
+
+
+#@pytest.mark.parametrize('group', test_data, ids=[repr(x) for x in test_data])
+
+def test_create_group(app, json_groups):
+    """Validation of correct create test group (All field fill up)"""
+    group = json_groups
+    old_groups = app.group.get_group_list()
+
+    app.group.create(group)
+    app.group.click_group_page()
+
+    assert len(old_groups)+1 == app.group.count(), 'Group does not created'
+
+    new_groups = app.group.get_group_list()
+    old_groups.append(group)
+    app.group.delete_first_group()
+    assert sorted(old_groups, key=Group.if_or_max) == sorted(new_groups, key=Group.if_or_max), 'Group list is different'
+
+
+def test_edit_group(app):
+    """Validation of correct edit group (all field updated)"""
+
+    app.group.validation_of_group_exist()
+    old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
+
+    group = Group(group_name=Profinity.long_word_20, group_header=Profinity.long_word_20,
+                  group_footer=Profinity.long_word_20)
+    group.id = old_groups[index].id
+    app.group.edit_group_by_index(group, index)
+
+    assert len(old_groups) == app.group.count(), 'Group list changed'
+
+    new_groups = app.group.get_group_list()
+    app.group.delete_first_group()
+    old_groups[index] = group
+    assert sorted(old_groups, key=Group.if_or_max) == sorted(new_groups, key=Group.if_or_max), 'Group list is different'
+
+
+def test_delete_group(app):
+    """Validation of correct delete group"""
+
+    app.group.validation_of_group_exist()
+    old_groups = app.group.get_group_list()
+
+    index = randrange(len(old_groups))
+    app.group.delete_group_by_index(index)
+
+    assert len(old_groups)-1 == app.group.count(), 'Group does not created'
+    new_groups = app.group.get_group_list()
+    old_groups[index:index+1] = []
+    assert old_groups == new_groups, 'Group list is different'
